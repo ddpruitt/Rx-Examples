32f83c877e1bc4f3d2d41db9e59769717dc77368
darren_pruitt <darren.pruitt@gmail.com>
2016-01-29 14:08:06 -0600

    Added file stream example

---
+++ DC.RxExamples/DC.RxExamples.Streamers/OStream.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reactive.Linq;
+
+namespace DC.RxExamples.Streamers
+{
+    public class OStream
+    {
+        public static void ObserveFileStream()
+        {
+            var source = new FileInfo(@".\Data\Numbers.txt");
+
+            var stream = ObserveLines(source.FullName);
+            stream.Subscribe(Console.WriteLine,             // onNext Action
+                ex => Console.WriteLine("Error: {0}", ex),  // onError Action
+                () => Console.WriteLine("Finished"));       // onComplete Action
+        }
+
+        public static IEnumerable<string> ReadLines(string source)
+        {
+            using (var reader = new StreamReader(source))
+            {
+                while (!reader.EndOfStream)
+                    yield return reader.ReadLine();
+            }
+        }
+
+        public static IObservable<string> ObserveLines(string source)
+        {
+            return ReadLines(source).ToObservable();
+        }
+    }
+}
---
+++ DC.RxExamples/DC.RxExamples.Streamers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DC.RxExamples.Streamers
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            OStream.ObserveFileStream();
+        }
+
+        
+    }
+}
